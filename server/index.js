36a82f07d1c46347301cb4e3820909b0638e679a
chakradha <chakradharghute@gmail.com>
2022-11-18 01:03:40 +0530

    added database connectivity

--- server/index.js
+++ server/index.js
@@ -1,7 +1,11 @@
 //const express = require('express');
 import express from 'express';
 
+
+import connection from './database/db.js'
 const app =express();
 
 const PORT =8000;
+
+connection();
 app.listen(PORT, () => console.log("server is running "));
